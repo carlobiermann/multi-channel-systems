44b8a28328ffb3e0a222258cda88df72627d62da
Carlo Biermann <39556873+carlobiermann@users.noreply.github.com>
2020-05-06 01:15:52 +0200

    Add files via upload

---
+++ simulation.m
@@ -0,0 +1,9 @@
+% radio communication system simulation script
+%
+
+clc; clear variables; 
+addpath('subfunctions');
+
+% global simulation parameters 
+
+generateBits(30);
---
+++ subfunctions/quadMean.m
@@ -0,0 +1,5 @@
+function y = quadMean(x)
+
+    y = mean(x.^2);
+    
+end
