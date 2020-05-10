a847b8b48606bb236f2d096a46f78ca161a8a6bf
Carlo Biermann <39556873+carlobiermann@users.noreply.github.com>
2020-05-10 22:49:31 +0200

    Add files via upload
    updated

---
+++ simulation.m
@@ -0,0 +1,16 @@
+% radio communication system simulation script
+
+clc; clear variables; 
+addpath('subfunctions');
+
+% global simulation parameters 
+const = [-1-1j, 1-1j, -1+1j, 1+1j]; % BPSK with Gray Code
+
+% create bit sequence
+bits = generateBits(18);
+
+% map the bit sequence to an array of corresponding modulation signs
+r = mapper(bits,const);
+
+
+
