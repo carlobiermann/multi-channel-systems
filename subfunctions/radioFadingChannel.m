9da620b91035a360bba9bc3bd3d38091e185771d
Carlo Biermann <39556873+carlobiermann@users.noreply.github.com>
2020-06-23 11:10:51 +0200

    Add files via upload

--- subfunctions/radioFadingChannel.m
+++ subfunctions/radioFadingChannel.m
@@ -1,9 +1,17 @@
 % This function returns the normalized complex coefficients of a Rayleigh
 % channel
 
-function y = radioFadingChannel(nSample)
+function y = radioFadingChannel(nSample, K)
 
-h = randn(1, nSample) + 1i*randn(1, nSample);
+hNLOS = randn(1, nSample) + 1i*randn(1, nSample);
+hNLOS = setMeanPower(hNLOS,1);
+pNLOS = quadMean(hNLOS); 
+
+hLOS = 1 * exp(1i*2*pi*randn(1, nSample));
+pLOS = K * pNLOS; 
+hLOS = setMeanPower(hLOS, pLOS);
+
+h = hLOS + hNLOS;
 
 y = setMeanPower(h, 1);
 
